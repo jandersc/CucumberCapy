7a794f35b62ecda003ca451ae47c75e6fd1049e8
Jander Silva de Cerqueira <jscerqueira@stefanini.com>
2020-09-21 18:48:53 -0300

    aula concluida do QANinja

--- features/support/pages/movie_page.rb
+++ features/support/pages/movie_page.rb
@@ -1,25 +1,34 @@
-class MoviePage
-    include Capybara::DSL
+class MoviePage #nova classe para a página Movie
+    include Capybara::DSL #incluindo o capybara ao arquivo
 
-    def add
+    def add #metódo que clica no botão + para add um novo filme
         find(".nc-simple-add").click
     end
 
-    def create(movie)
+    def create(movie) #metódo que cria um novo filme pegando os valores do arquivo YAML
+        
+        #preenchendo o campo filme
         find('input[name=title]').set movie["title"]
         
-        #combobox
-        find('input[placeholder=Status]').click
-        find(".el-select-dropdown__item", text: movie["status"]).click
+        #preenche o combobox
+        find('input[placeholder=Status]').click #necessário o click para habilitar os campos
+        find(".el-select-dropdown__item", text: movie["status"]).click #seleciona o status do filme
 
-        find('input[name=year]').set movie["year"]
+        #preenche o ano de lançamento
+        find('input[name=year]').set movie["year"] 
 
+        #preenche a data de lançamento
         find('input[name=release_date]').set movie["release_date"]
 
-        actor = find(".input-new-tag")
-        movie['cast'].each do |a|
-            actor.set a
-            actor.send_keys :tab
+        #seleciona os atores
+        actor = find(".input-new-tag") #atribuindo a variável o campo de ator
+        movie['cast'].each do |a| #ler o array do arquivo e atribui a variável 
+            actor.set a #altera o campo com valor da variável
+            actor.send_keys :tab #TAB para add o próximo ator do array
         end
+
+        #preenche o campo com a sinopse do filme 
+        find('textarea[name=overview]').set movie["overview"]
+
     end
 end
