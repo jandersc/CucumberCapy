3fe00d6a73e6419309abd4bd9fa25a7b75bb7f06
Jander Silva de Cerqueira <jscerqueira@stefanini.com>
2020-09-23 19:14:32 -0300

    Validando campos obrigatórios

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,17 +1,21 @@
 Dado('que {string} é um novo filme') do |movie_code|
     file = YAML.load_file(File.join(Dir.pwd, 'features/support/fixtures/movies.yaml')) #procurar o arquivo yaml na raiz do cucumber
     @movie = file[movie_code]
-  end
+end
   
-  Quando('eu faço o cadastro deste filme') do
+Quando('eu faço o cadastro deste filme') do
     @movie_page.add
     @movie_page.create(@movie)
     #sleep 3
-  end
+end
   
-  Então('devo ver o novo filme na lista') do
+Então('devo ver o novo filme na lista') do
     #compara se o retorno da tabela está igual ao arquivo YAML
     resultado = @movie_page.movie_tr(@movie)
     expect(resultado).to have_text @movie["title"]
     expect(resultado).to have_text @movie["status"]
-  end
+end
+
+Então('devo ver a notificação {string}') do |expect_alert|
+    expect(@movie_page.alert).to eql expect_alert
+end
--- features/support/pages/movie_page.rb
+++ features/support/pages/movie_page.rb
@@ -5,14 +5,31 @@ class MoviePage #nova classe para a página Movie
         find(".movie-add").click
     end
 
+
+    def upload(file)
+         #procurar o diretório onde tá a imagem e atribui a variável
+         cover_file = File.join(Dir.pwd, "features/support/fixtures/cover/" + file)
+         #comando para o Capybara ingnorar os elementos desabilitados (o elemento "upcover está desabilitado")
+         Capybara.ignore_hidden_elements = false
+         #adiciona o arquivo ao elemento de upload de imagem 
+         attach_file('upcover', cover_file) 
+         #comando para voltar a considerar os elementos desabilitados.
+         Capybara.ignore_hidden_elements = true       
+    end
+
+
+    def select_status(status)
+        #preenche o combobox
+        find('input[placeholder=Status]').click #necessário o click para habilitar os campos
+        find(".el-select-dropdown__item", text: status).click #seleciona o status do filme
+    end
+
     def create(movie) #metódo que cria um novo filme pegando os valores do arquivo YAML
         
         #preenchendo o campo filme
         find('input[name=title]').set movie["title"]
         
-        #preenche o combobox
-        find('input[placeholder=Status]').click #necessário o click para habilitar os campos
-        find(".el-select-dropdown__item", text: movie["status"]).click #seleciona o status do filme
+        select_status(movie["status"]) unless movie["status"].empty? #utiliza o unless para não executar o attach se estiver vazio
 
         #preenche o ano de lançamento
         find('input[name=year]').set movie["year"] 
@@ -30,17 +47,7 @@ class MoviePage #nova classe para a página Movie
         #preenche o campo com a sinopse do filme 
         find('textarea[name=overview]').set movie["overview"]
 
-        #procurar o diretório onde tá a imagem e atribui a variável
-        cover_file = File.join(Dir.pwd, "features/support/fixtures/cover/" + movie["cover"])
-
-        #comando para o Capybara ingnorar os elementos desabilitados (o elemento "upcover está desabilitado")
-        Capybara.ignore_hidden_elements = false
-
-        #adiciona o arquivo ao elemento de upload de imagem 
-        attach_file('upcover', cover_file)
-        
-        #comando para voltar a considerar os elementos desabilitados.
-        Capybara.ignore_hidden_elements = true
+        upload(movie["cover"]) unless movie["cover"].empty? #utiliza o unless para não executar o attach se estiver vazio
 
         #clicar no botão cadastrar
         find("#create-movie").click
@@ -49,5 +56,9 @@ class MoviePage #nova classe para a página Movie
     #metódo que procura os elementos da tabela com base nos titulos
     def movie_tr(movie)
       find('table tbody tr', text: movie["title"])  
+    end
+
+    def alert
+        find(".alert").text
     end
 end
