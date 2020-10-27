a27ddd6cc56c681d0d3af6c854f17aba64540553
Jander Silva de Cerqueira <jscerqueira@stefanini.com>
2020-10-27 18:50:09 -0300

    Anexando evidências no relatório

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'capybara'
-gem 'cucumber'
+gem 'cucumber', '3.1.2'
 gem 'rspec'
 gem 'selenium-webdriver'
 gem 'pg' #para conectar no banco postgres
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -12,3 +12,11 @@ Before("@login") do
     @login_page.go
     @login_page.with(user["email"], user["pass"])
 end
+
+After do |scenario|
+    #if scenario.failed?  #usado para tirar print apenas quando o cenário falhar
+    temp_shot = page.save_screenshot("log/temp_shot.png") #adiciona na variável o print da ultima tela do cenário
+    screenshot = Base64.encode64(File.open(temp_shot, "rb").read) #converte a imagem e armazena em uma nova variável
+    embed(screenshot, "image/png", "Screenshot") #anexa os prints no relatório
+    #end
+end
