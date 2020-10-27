79544855b45836f6778d624cc095b353a5647616
Jander Silva de Cerqueira <jscerqueira@stefanini.com>
2020-10-27 19:52:12 -0300

    Aula bônus Report Builder S2

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'http://rubygems.org'
 
 gem 'capybara'
 gem 'cucumber', '3.1.2'
+gem 'ffi'
 gem 'rspec'
 gem 'selenium-webdriver'
 gem 'pg' #para conectar no banco postgres
+gem 'report_builder' #gerar o relatório report builder
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,3 +1,6 @@
+require 'report_builder' #add a bibloteca do report_builder
+require 'date' #add a biblioteca de data para DateTime.now
+
 Before do #inserir o as variáveis para todo o projeto
     @login_page = LoginPage.new
     @movie_page = MoviePage.new
@@ -19,4 +22,20 @@ After do |scenario|
     screenshot = Base64.encode64(File.open(temp_shot, "rb").read) #converte a imagem e armazena em uma nova variável
     embed(screenshot, "image/png", "Screenshot") #anexa os prints no relatório
     #end
+end
+
+d = DateTime.now #chama o método para pegar hora atual   
+@current_date = d.to_s.tr(':', '-') #troca os dois pontos por traço
+
+at_exit do
+    ReportBuilder.configure do |config|
+        config.input_path = 'log/report.json'
+        config.report_path = "log/" + @current_date
+        config.report_types = [:html]
+        config.report_title = 'NinjaFlix - WebApp'
+        config.compress_image = true
+        config.additional_info = {'App' => 'Web', 'Data de Execução' => @current_date}
+        config.color = 'indigo'
+    end
+    ReportBuilder.build_report 
 end
